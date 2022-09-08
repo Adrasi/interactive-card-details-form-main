d90bb4a5dd23e371e0aaeac6e721db6bb3624679
Muhammad Salisu Ishaq <adramuhammad3@gmail.com>
2022-09-08 13:51:26 +0100

    Added Javascript

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-function myFunc() {
-    let x = document.form["form"]["cardholder", "cardnumber", "expdate", "cvc"].value;
-    if (x = "") {
-        alert("This field is required");
-        return false;
-    }
+// function myFunc() {
+//     let x = document.form["form"]["cardholder", "cardnumber", "expdate", "cvc"].value;
+//     if (x = "") {
+//         alert("This field is required");
+//         return false;
+//     }
+// }
+
+const cardNumber = document.querySelector('.number-div');
+const cardHolderName = document.querySelector('.name-div');
+const inputCardHolder = document.getElementById('cardholder');
+const inputCardNumber = document.getElementById('cardnumber');
+
+
+const val = e => {
+    cardNumber.textContent = e
+}
+
+const val2 = e => {
+    cardHolderName.textContent = e
 }
